d681f79f7c5a145ec52f5980ab38ec65f342833b
aiglesiasp <iglesiaspubillaitor@gmail.com>
2022-12-01 14:49:34 +0100

    Initial project
    Project initial with SwiftUI

---
+++ FirstProjectSwiftUI/FirstProjectSwiftUI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  FirstProjectSwiftUI
+//
+//  Created by Aitor Iglesias Pubill on 1/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FirstProjectSwiftUI/FirstProjectSwiftUI/FirstProjectSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstProjectSwiftUIApp.swift
+//  FirstProjectSwiftUI
+//
+//  Created by Aitor Iglesias Pubill on 1/12/22.
+//
+
+import SwiftUI
+
+@main
+struct FirstProjectSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
