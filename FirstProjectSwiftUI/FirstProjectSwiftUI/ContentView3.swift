a1644c68fa47bdfc5e3b48c921f7c3aedbde2d12
aiglesiasp <iglesiaspubillaitor@gmail.com>
2022-12-01 22:25:20 +0100

    Usando 2 STACKS

---
+++ FirstProjectSwiftUI/FirstProjectSwiftUI/ContentView3.swift
@@ -0,0 +1,34 @@
+//
+//  ContentView3.swift
+//  FirstProjectSwiftUI
+//
+//  Created by Aitor Iglesias Pubill on 1/12/22.
+//
+
+import SwiftUI
+
+struct ContentView3: View {
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Nombre")
+                Text("Keppcoder")
+            }
+            .padding()
+            .background(Color.red)
+    
+            HStack {
+                Text("Apellidos")
+                Text("Keepcoder")
+            }
+            .padding()
+            .background(Color.yellow)
+        }
+    }
+}
+
+struct ContentView3_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView3()
+    }
+}
