93520db6130fd107eefbdc912d14c2f4f8466f80
aiglesiasp <iglesiaspubillaitor@gmail.com>
2022-12-02 20:16:39 +0100

    Lista con Navigation

--- FirstProjectSwiftUI/FirstProjectSwiftUI/ContentView4_5.swift
+++ FirstProjectSwiftUI/FirstProjectSwiftUI/ContentView4_5.swift
@@ -9,16 +9,34 @@ import SwiftUI
 
 struct ContentView4_5: View {
     
-    let bestFriends = ["Juan", "Maria", "Paco", "Luis"]
+    @State var bestFriends = ["Juan", "Maria", "Paco", "Luis"]
     var body: some View {
         NavigationView {
             List {
                 ForEach(bestFriends, id: \.self) { friends in
                     Text("\(friends)")
+                        .swipeActions(edge: .trailing, allowsFullSwipe: true) {
+                            //botones para cada accion
+                            Button {
+                                //Action here
+                            } label: {
+                                Label {
+                                    Text("Silenciar")
+                                } icon: {
+                                    Image(systemName: "bell.slash")
+                                }
+                            }
+                            .tint(.red)
+
+                        }
                 }
             }
             .navigationTitle("MIS AMIGOS")
             .navigationBarTitleDisplayMode(.inline)
+            .refreshable {
+                //pull to refresh
+                bestFriends.append("Nuevo amigo")
+            }
         }
     }
 }
